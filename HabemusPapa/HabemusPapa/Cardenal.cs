6f648001300fb62b1e5c86d60f6db2e7023e4fe6
Gusrot <gnrotela@yahoo.com.ar>
2016-09-28 11:18:05 -0300

    Add files via upload

---
+++ HabemusPapa/HabemusPapa/Cardenal.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HabemusPapa
+{
+    public class Cardenal
+    {
+        private int _cantVotosRecibidos;
+        private ENacionalidades _nacionalidad;
+        private string _nombre;
+        private string _nombrePapal;
+    
+        private Cardenal()
+        {
+            this._cantVotosRecibidos = 0;
+        }
+
+        public Cardenal(string nombre, string nombrePapal)
+        {
+            this._nombre = nombre;
+            this._nombrePapal = nombrePapal;
+        }
+
+        public Cardenal(string nombre, string nombrePapal, ENacionalidades nacionalidad):this(nombre,nombrePapal)
+        {
+            this._nacionalidad = nacionalidad;
+        }
+
+        public int getCantidadVotosRecibidos()
+        {
+            return _cantVotosRecibidos;
+        }
+
+        private string Mostrar()
+        {
+            string nombre = ObtenerNombreYNombrePapal();
+
+            StringBuilder sb = new StringBuilder();
+            
+            sb.AppendLine(nombre);
+            sb.AppendLine("Nacionalidad: " + this._nacionalidad);
+            sb.AppendLine("Cantidad de votos: " + this._cantVotosRecibidos);
+
+            return sb.ToString();
+        }
+
+        public string Mostrar(Cardenal c)
+        {
+            string cardenal;
+
+            cardenal = c.Mostrar();
+
+            return cardenal;
+        }
+
+        public string ObtenerNombreYNombrePapal()
+        {
+            string papa;
+
+            papa=("El cardenal " + this._nombre + " se llamará " + this._nombrePapal + ".");
+
+            return papa;
+        }
+
+        public static bool operator ==(Cardenal c1, Cardenal c2)
+        {
+            if (c1._nombre == c2._nombre && c1._nombrePapal == c2._nombrePapal && c1._nacionalidad == c2._nacionalidad)
+                return true;
+            return false;
+        }
+
+        public static bool operator !=(Cardenal c1, Cardenal c2)
+        {
+            return !(c1 == c2);
+        }
+
+        public static Cardenal operator ++(Cardenal c)
+        {
+            c._cantVotosRecibidos += 1;
+
+            return c;
+        }
+    }
+}
---
+++ HabemusPapa/HabemusPapa/Conclave.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HabemusPapa
+{
+    public class Conclave
+    {
+        private int _cantMaxCardenales;
+        private List<Cardenal> _cardenales;
+        private bool _habemusPapa;
+        private string _lugarEleccion;
+        private Cardenal _papa;
+        static int cantidadVotaciones;
+        static DateTime fechaVotacion;
+
+        public Conclave()
+        {
+            this._cantMaxCardenales = 1;
+            this._lugarEleccion = ("Capilla Sixtina");
+        }
+
+        static Conclave()
+        {
+            cantidadVotaciones = 0;
+            fechaVotacion = DateTime.Now;
+        }
+
+        private Conclave(int cantidadCardenales):this()
+        {
+            this._cantMaxCardenales = cantidadCardenales;
+        }
+
+        public Conclave(int cantidadCardenales, string lugarEleccion):this(cantidadCardenales)
+        {
+            this._lugarEleccion = lugarEleccion;
+        }
+
+        private void ContarVotos(Conclave conclave)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private bool HayLugar()
+        {
+            if (this._cantMaxCardenales < this._cardenales.Count)
+                return true;
+            return false;
+        }
+
+        public string Mostrar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private string MostrarCardenales()
+        {
+            string cardenal;
+
+            foreach (Cardenal item in this._cardenales)
+            {
+                cardenal=item.Mostrar(item);
+            }
+
+            return cardenal;
+        }
+
+        public void VotarPapa(Conclave conclave)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static bool operator ==(Conclave con, Cardenal c)
+        {
+            foreach (Cardenal item in con._cardenales)
+            {
+                if (item.Equals(c))
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Conclave con, Cardenal c)
+        {
+            return !(con == c);
+        }
+
+    }
+}
